bb04934131f6e3b0e77bc6cdf0c4e69622b3b694
Andrew-Sem <avvyayvayva@gmail.com>
2023-02-06 21:17:50 +0300

    getByText test

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -1,14 +1,10 @@
 import { render, screen } from "@testing-library/react";
-import { Greet } from "./greet";
+import { Greet } from "./Greet";
 
-test("Greet renders correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText("Hello");
-  expect(textElement).toBeInTheDocument();
-});
-
-test("Greet renders with a name", () => {
-  render(<Greet name="Andrew" />);
-  const textElement = screen.getByText("Hello Andrew");
-  expect(textElement).toBeInTheDocument();
+describe("Greet", () => {
+  test("Greet renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/Hello/);
+    expect(textElement).toBeInTheDocument();
+  });
 });
